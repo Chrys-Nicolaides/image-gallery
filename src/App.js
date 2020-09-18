ce9711b6488c3e349908cd74488d505e9c57a196
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2020-09-17 22:05:54 -0700

    tailwind and dark theme exercise

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React, { useState, useEffect } from 'react'; 
-import ImageCard from './components/ImageCard';
-import ImageSearch from './components/ImageSearch';
-
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
 
-console.log(process.env.REACT_APP_PIXABAY_API_KEY);
+  console.log(process.env.REACT_APP_PIXABAY_API_KEY);
 
   useEffect(() => {
-    fetch(`http://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
-    .then(res => res.json())
-    .then(data => {
-      setImages(data.hits);
-      setIsLoading(false);
-    })
-    .catch(err => console.log(err));
+    fetch(
+      `http://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
   }, [term]);
 
   return (
-   <div className="container mx-auto">
-      <ImageSearch searchText={(text) => setTerm(text)} />
-      
-      {!isLoading && images.length === 0 && <h1 className="text-4xl text-center text-gray-900 mx-auto">No Images Found :(</h1>}
+    <div>
+      <div className="container mx-auto">
+        <ImageSearch searchText={(text) => setTerm(text)} />
 
-      {isLoading ? <h1 className="text-4xl text-center mx-auto mt-32">Loading...</h1> :
-        <div className="grid grid-cols-3 gap-4">
-          {images.map(image => (
-         <ImageCard key={image.id} image={image} />
-       ))}
-     </div>}
-   </div>
+        {!isLoading && images.length === 0 && (
+          <h1 className="text-4xl text-center text-gray-900 mx-auto">
+            No Images Found :(
+          </h1>
+        )}
+
+        {isLoading ? (
+          <h1 className="text-4xl text-center mx-auto mt-32">Loading...</h1>
+        ) : (
+          <div className="grid grid-cols-3 gap-4">
+            {images.map((image) => (
+              <ImageCard key={image.id} image={image} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
 export default App;
- 
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React from "react";
 
-const ImageCard = ({image}) => {
-    const tags = image.tags.split(',');
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
 
-    return (
-        <div className="max-w-sm rounded-lg overflow-hidden shadow-lg mt-6 ml-6">
-        <img src={image.webformatURL} alt="" 
-        className="w-full"/>
-        <div className="px-6 py-4">
-          <div className="font-bold text-indigo-600 text-xl mb-2">
-            Photo by {image.user}
-          </div>
-          <ul>
-            <li>
-              <strong>Views: </strong>
-               {image.views}
-            </li>
-            <li>
-              <strong>Downloads: </strong>
-              {image.downloads}
-            </li>
-            <li>
-              <strong>Likes: </strong>
-              {image.likes}
-            </li>
-          </ul>
+  return (
+    <div className="max-w-lg rounded-lg overflow-hidden shadow-lg mt-8 ml-8 bg-gray-900">
+      <img src={image.webformatURL} alt="" className="w-full" />
+      <div className="px-6 py-4 text-gray-500">
+        <div className="font-bold text-gray-100 text-xl mb-2">
+          Photo by {image.user}
         </div>
-        <div className="px-6 py-4">
-         {tags.map((tag, index) => (
-            <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #{tag}
-            </span>
-         ))}
-        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
       </div>
-    )
-}
+      <div className="px-6 py-4">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-indigo-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 m-1"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default ImageCard;
